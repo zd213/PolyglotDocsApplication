5bb7c0d907fff92941c6d95b453ecae4d754e61e
zahra <z.dorosti213@gmail.com>
2024-10-18 00:02:21 +0330

    add swagger ui

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 > mvn clean package
 
 > sudo docker compose up --build
+
+Web UI on: http://localhost:8080 (this has some problem in creating new documents, but there is another way, YES swagger UI)
+
+Swagger On: http://localhost:8080/swagger-ui
---
+++ src/main/java/dev/arusha/PolyglotDocs/config/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package dev.arusha.PolyglotDocs.config;
+
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
